3d8ebfc85ae86dee96dd2899d2e6e42dc7cd5d4b
himani472 <hhimani472@gmail.com>
2020-04-11 19:27:00 +0530

    remove ngOnInit

--- src/app/regestration-form/regestration-form.component.ts
+++ src/app/regestration-form/regestration-form.component.ts
@@ -12,16 +12,7 @@ export class RegestrationFormComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      firstName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-      lastName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-      email: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-      Password: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-      number: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-     
-    });
-  }
+  
 
   onSubmit() {
     console.log(this.registerForm);
